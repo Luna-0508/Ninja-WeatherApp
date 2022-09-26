b49dfa3b495351b3ba14fb808b115cd50b5b6b2b
Natalia Łukaszka <lukaszkanatalia94@gmail.com>
2022-09-26 21:09:00 +0200

    connect API

---
+++ forecast.js
@@ -0,0 +1,15 @@
+const key = "UIt1ZhFuzLLtPMbeQiqx6bjeALTxkiC9";
+
+const getCity = async () => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${key}&q${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+getCity("manchester")
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
