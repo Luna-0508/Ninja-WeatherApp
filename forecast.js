92ec175d45e76d894102a7204b97621c147e9c6c
Natalia Łukaszka <lukaszkanatalia94@gmail.com>
2022-09-26 21:21:54 +0200

    Add weather condition API

--- forecast.js
+++ forecast.js
@@ -1,5 +1,17 @@
 const key = "UIt1ZhFuzLLtPMbeQiqx6bjeALTxkiC9";
 
+// get weather information
+const getWeather = async id => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+// get city information
 const getCity = async () => {
   const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
   const query = `?apikey=${key}&q${city}`;
@@ -11,5 +23,10 @@ const getCity = async () => {
 };
 
 getCity("manchester")
-  .then(data => console.log(data))
+  .then(data => {
+    return getWeather(data.Key);
+  })
+  .then(data => {
+    console.log(data);
+  })
   .catch(err => console.log(err));
